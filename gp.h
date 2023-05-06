f14428d243ac928d5804cd21b58e488644846b26
DreamMaoMao <2523610504@qq.com>
2023-05-06 16:35:37 +0800

    删除多余代码

--- gp.h
+++ gp.h
@@ -11,13 +11,6 @@
 #define HTTP_VERSION "HTTP/1.1"
 #define CRLF "\r\n" // 回车换行符
 #define BUFFER_LEN 40960 //返回体接受的缓冲区长度，太小接受不了过长的返回
-// 定义一些常量
-#define GP_HTTP_GET "GET"
-#define GP_HTTP_POST "POST"
-#define GP_HTTP_VERSION "HTTP/1.1"
-#define GP_CRLF "\r\n" // 回车换行符
-#define GP_RESPONSE_BUFFER_LEN 40960 //返回体接受的缓冲区长度，太小接受不了过长的返回
-
 
 struct gp_http_response {  //定义响应体
     char *body;
